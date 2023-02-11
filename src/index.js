f78c8aa917738182064bdd284f146b721957fd58
lwuite <125051724+lwuite@users.noreply.github.com>
2023-02-11 02:46:49 +0100

    Add a responsive nav menu #479 (#2)
    https://github.com/netlify-templates/one-click-hugo-cms/pull/479
    
    Co-authored-by: kwuite <kelvin@ludaro.nl>

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 
 import "./css/main.scss";
 
+// https://github.com/netlify-templates/one-click-hugo-cms/pull/479
+const mobileMenu = document.querySelector('[data-mobile-menu]');
+const nav = document.querySelector('[data-nav]');
+
+function toggleMobileMenu() {
+    nav.classList.toggle('menu-open');
+}
+
+mobileMenu.addEventListener('click', toggleMobileMenu);
+
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
